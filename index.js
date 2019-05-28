40fbdc8288edb9ec2ee21998c098f816684e5473
cekongsb <806138244@qq.com>
2019-05-28 10:30:02 +0800

    modify 'Particl' to 'Vpub' not whole word

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ bitcore.versionGuard = function(version) {
     throw new Error(message);
   }
 };
-bitcore.versionGuard(global._bitcoreParticl);
-global._bitcoreParticl = bitcore.version;
+bitcore.versionGuard(global._bitcoreVpub);
+global._bitcoreVpub = bitcore.version;
 
 // crypto
 bitcore.crypto = {};
